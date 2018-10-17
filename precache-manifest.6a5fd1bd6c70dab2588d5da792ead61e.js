e2517a7262f7cd7410807d71f8fb7d8c53cc6d5f
monikadembek <monika.dembek@gmail.com>
2018-10-17 21:24:58 +0200

    Updates

---
+++ precache-manifest.6a5fd1bd6c70dab2588d5da792ead61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-first/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d89adfcaf7e453c5ec9",
+    "url": "/react-first/static/js/runtime~main.8d89adfc.js"
+  },
+  {
+    "revision": "ed0c7ee806c1ee5f9656",
+    "url": "/react-first/static/js/main.ed0c7ee8.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/react-first/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "ed0c7ee806c1ee5f9656",
+    "url": "/react-first/static/css/main.e71447f0.chunk.css"
+  },
+  {
+    "revision": "913d9a3c5b84a75657c1faa4ebf6fe08",
+    "url": "/react-first/index.html"
+  }
+];
